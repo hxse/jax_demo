1cdf453e942b8c963bfd5acde7298a7c9b0bbb63
hxse <qmlibulqxk@gmail.com>
2025-07-19 11:40:34 +0800

    udpate

--- src/examples/run_simple_benchmark.py
+++ src/examples/run_simple_benchmark.py
@@ -8,12 +8,24 @@ micro_path = "database/live/BTC_USDT/15m/BTC_USDT_15m_20230228 160000.csv"
 macro_path = "database/live/BTC_USDT/4h/BTC_USDT_4h_20230228 160000.csv"
 num = 2
 data_size = 40 * 1000
+cpu_unroll = 5
+gpu_unroll = 9
+enable_cpu = True
+enable_gpu = True
+enable_run_second = True
 
 
-def main(micro_path: str = micro_path,
-         macro_path: str = macro_path,
-         num: int = num,
-         data_size: int = data_size):
+def main(
+    micro_path: str = micro_path,
+    macro_path: str = macro_path,
+    data_size: int = data_size,
+    cpu_unroll: int = cpu_unroll,
+    gpu_unroll: int = gpu_unroll,
+    enable_cpu: bool = enable_cpu,
+    enable_gpu: bool = enable_gpu,
+    enable_run_second: bool = enable_run_second,
+    num: int = num,
+):
     configure_jax_cache(True)
     cpu_devices, gpu_devices = print_jax_device_info()
 
@@ -25,16 +37,23 @@ def main(micro_path: str = micro_path,
 
     result = run_benchmark(np_data,
                            np_data2=np_data2,
-                           cpu_unroll=5,
-                           gpu_unroll=9,
-                           enable_cpu=True,
-                           enable_gpu=True,
-                           enable_run_second=True,
+                           cpu_unroll=cpu_unroll,
+                           gpu_unroll=gpu_unroll,
+                           enable_cpu=enable_cpu,
+                           enable_gpu=enable_gpu,
+                           enable_run_second=enable_run_second,
                            num=num)
     print([k for k, v in result.items() if v != None])
-    print(
-        "sma_result length",
-        len(result["cpu_result"]["micro"]["indicators"]["sma"]["sma_result"]))
+    if enable_cpu:
+        print(
+            "sma_result length",
+            len(result["cpu_result"]["micro"]["indicators"]["sma"]
+                ["sma_result"]))
+    if enable_gpu:
+        print(
+            "sma_result length",
+            len(result["gpu_result"]["micro"]["indicators"]["sma"]
+                ["sma_result"]))
 
 
 if __name__ == "__main__":
